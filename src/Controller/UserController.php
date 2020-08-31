db955ec51958414123c23997e144be09a84f07d8
Noctumsempra <lcomerci@kiusys.com>
2020-08-31 08:33:19 -0300

    User creation implemented.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,19 +2,67 @@
 
 namespace App\Controller;
 
+use Exception;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
     /**
      * @Route("/create_user", name="POST_create_user", methods={"POST"})
+     * @param EntityManagerInterface $entityManager
+     * @param UserRepository $userRepository
+     *
+     * @return JsonResponse
+     * @throws Exception
      */
-    public function postCreateUser()
+    public function postCreateUser(EntityManagerInterface $entityManager, UserRepository $userRepository)
     {
+        $request = Request::createFromGlobals();
+
+        $json_data = json_decode($request->getContent());
+
+        // Sanitizo datos
+        $json_data->name = isset($json_data->name) ? trim($json_data->name) : '';
+        $json_data->phone = isset($json_data->phone) ? trim($json_data->phone) : '';
+        $json_data->email = isset($json_data->email) ? trim($json_data->email) : '';
+
+        // Error si no especifican nombre
+        if (!$json_data->name) {
+            return $this->json(['success' => false, 'message' => 'Name cannot be empty.']);
+        }
+
+        // Error si no especifican email
+        if (!$json_data->email) {
+            return $this->json(['success' => false, 'message' => 'E-mail cannot be empty.']);
+        }
+
+        // Error si el usuario con ese email ya existe
+        $user = $userRepository->findBy(['email' => $json_data->email]);
+        if ($user) {
+            return $this->json(['success' => false, 'message' => 'E-mail already exists.']);
+        }
+
+        $newUser = new User();
+        $newUser->setName($json_data->name);
+        $newUser->setEmail($json_data->email);
+        $newUser->setPhone($json_data->phone);
+        $newUser->setCreatedAt(new \DateTime('now', new \DateTimeZone('America/Buenos_Aires')));
+
+        $entityManager->persist($newUser);
+        $entityManager->flush();
+
         return $this->json([
             'success' => true,
-            'message' => '/create_user'
+            'message' => [
+                'id' => $newUser->getId(),
+                'createdAt' => $newUser->getCreatedAt()->format('d-m-Y H:i:s')
+            ]
         ]);
     }
 }
