e11328336f510b2e14c6cfd982d2cbdf249acc24
Noctumsempra <lcomerci@kiusys.com>
2020-08-31 10:06:24 -0300

    Ford Cars listing implemented.

--- src/Controller/CarsController.php
+++ src/Controller/CarsController.php
@@ -3,18 +3,66 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class CarsController extends AbstractController
 {
+    private $httpClient;
+
+    public function __construct()
+    {
+        $this->httpClient = HttpClient::create([
+            'base_uri' => 'http://server.cocoche.com.ar',
+        ]);
+    }
+
     /**
      * @Route("/get_ford_cars", name="GET_ford_cars", methods={"GET"})
+     * @return JsonResponse
+     *
+     * @throws TransportExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws DecodingExceptionInterface
      */
     public function getFordCars()
     {
-        return $this->json([
-            'success' => true,
-            'message' => '/get_ford_cars'
-        ]);
+        try {
+            $response = $this->httpClient->request('GET', '/car_listing_presentation', [
+                'query' => ['list_length' => 100]
+            ]);
+            $contents = $response->toArray();
+            $cars = $contents['carList'];
+
+            $ford_cars = array_filter($cars, function($car) {
+                return strtoupper($car['brandDescription']) == 'FORD';
+            });
+
+            if (!$ford_cars) {
+                return $this->json([
+                    'success' => false,
+                    'message' => 'No FORD cars have been found.'
+                ]);
+            }
+
+            return $this->json([
+                'success' => true,
+                'message' => $ford_cars
+            ]);
+
+        } catch (\Exception $exception) {
+            return $this->json([
+                'success' => false,
+                'message' => $exception->getMessage()
+            ]);
+        }
     }
 }
